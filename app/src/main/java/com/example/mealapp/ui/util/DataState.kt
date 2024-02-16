3097117cc2e8f811f52591f5154c7d0e7b2d97e6
Heba-Alii <hebaaliali18@gmail.com>
2024-02-16 21:38:41 +0200

    Handle api errors

--- app/src/main/java/com/example/mealapp/ui/util/DataState.kt
+++ app/src/main/java/com/example/mealapp/ui/util/DataState.kt
@@ -3,7 +3,7 @@ package com.example.mealapp.ui.util
 sealed class DataState<out T> {
     data class Success<out T>(val data: T) : DataState<T>()
     data class Failure(val throwable: Throwable) : DataState<Nothing>()
-    data class Loading(val isLoading: Boolean, val text: String = "") : DataState<Nothing>()
+    data class Loading(val isLoading: Boolean) : DataState<Nothing>()
 }
 
 
